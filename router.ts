6d9a50f71fdc524a1279b0f188468175550cee0c
Guilherme Rodrigues <gui@futuredriven.dev>
2022-07-04 22:07:20 -0300

    Route to {name}.decocache.com

--- router.ts
+++ router.ts
@@ -1,31 +1,50 @@
 import { serve } from "https://deno.land/std@0.146.0/http/server.ts";
 import { createClient } from "supabase";
+import { WeakLRUCache } from "https://deno.land/x/weakcache@v1.1.4/index.js";
 
 const supabase = createClient(
   `https://${Deno.env.get("SUPABASE_ACCOUNT") as string}.supabase.co`,
   Deno.env.get("SUPABASE_KEY") as string,
 );
 
+// TODO
+const domainCache = new WeakLRUCache();
+
 const handler = async (req: Request) => {
   // Use the request pathname as filepath
   const url = new URL(req.url);
-  const filepath = decodeURIComponent(url.pathname.replace("/1", ""));
+  const path = decodeURIComponent(url.pathname);
 
-  // Try opening the file
-  let file;
-  try {
-    file = await Deno.open("." + filepath, { read: true });
-  } catch {
-    // If the file cannot be opened, return a "404 Not Found" response
-    return new Response("404 Not Found", { status: 404 });
-  }
+  console.log(url.hostname);
 
-  // Build a readable stream so the file doesn't have to be fully loaded into
-  // memory while we send it
-  const readableStream = file.readable;
+  let { data: Domains, error } = await supabase
+    .from("Domains")
+    .select(`
+      name,
+      site(
+        name
+      )`)
+    .eq("name", url.hostname);
 
-  // Return the file as a response
-  return new Response(readableStream);
+  const name = Domains![0].site.name;
+  console.log(Domains![0].site.name);
+
+  url.protocol = "https:";
+  url.hostname = `${name}.decocache.com`;
+  url.port = "443";
+
+  // Route request to rendering service for this site
+  return await fetch(url.href, {
+    headers: req.headers,
+    method: req.method,
+    body: req.body,
+  });
+
+  // return new Response(JSON.stringify(Domains), {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 };
 const port = parseInt(Deno.env.get("PORT") || "4201");
 
