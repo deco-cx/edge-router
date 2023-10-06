3a6e2b98e0828c4c2280a5c7ef507d9a67fd26b9
Marcos Candeia <marrcooos@gmail.com>
2023-10-05 21:13:24 -0300

    Add cache stale for pages (#1)
    * Initial commit (by create-cloudflare CLI)
    
    * Add stale for pages
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Add github deploy action
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ src/router.ts
@@ -0,0 +1,62 @@
+import {
+  cacheStaleFor,
+  ddUrl,
+  getETag,
+  isBrowserRequestingPages,
+} from "./commons.ts";
+
+const cache = caches.default;
+const CACHE_TIME_MAX_SECONDS = 4;
+const CACHE_TIME_MIN_SECONDS = 1;
+
+// Export a default object containing event handlers
+export default {
+  async fetch(
+    request: Request,
+    _env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const targetUrl = ddUrl(request);
+
+    if (!isBrowserRequestingPages(request)) {
+      return fetch(targetUrl, request);
+    }
+
+    const cacheStale = cacheStaleFor(
+      CACHE_TIME_MIN_SECONDS +
+        (Math.floor(Math.random() * CACHE_TIME_MAX_SECONDS)),
+      cache,
+      ctx,
+    );
+
+    const requestETag = getETag(request);
+    let response = requestETag ? await cache.match(requestETag) : undefined;
+    if (!response) {
+      // If not in cache, get it from origin
+      const head = new Request(targetUrl, {
+        method: "HEAD",
+        headers: request.headers,
+      });
+      const fetchAndCache = fetch(targetUrl, request).then(cacheStale);
+      response = await Promise.race([
+        fetch(head).then(async (response) => {
+          const responseETag = getETag(response);
+          // if does not have etag on header, then wait for the fetched response
+          if (!responseETag) {
+            return fetchAndCache;
+          }
+          // otherwise check if the response is cached and get the fastest response from either cached or fetched response.
+          return await Promise.race([
+            fetchAndCache,
+            cache.match(responseETag).then((response) => {
+              return response ?? fetchAndCache;
+            }),
+          ]);
+        }),
+        fetchAndCache,
+      ]);
+    }
+
+    return response;
+  },
+};
